77555a2cd4b9d94871f37166e1a3d254601f6d22
Panhchna04 <mpanhchna@gmail.com>
2023-11-18 01:33:19 +0700

    Sideber vue

---
+++ src/components/sidebar.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+<template>
+    <div class="sidebar">
+        hello sidebar
+        <!-- <ul>
+            <li><a href="">Home </a></li>
+            <li><a href="">About </a></li>
+            <li><a href="">Home </a></li>
+            <li><a href="">Home </a></li>
+            <li><a href="">Home </a></li>
+        </ul> -->
+    </div>
+</template>
+<style scoped>
+    .sidebar{
+        width: 10%;
+        height: 100vh;
+        background-color: aqua;
+    }
+</style>
